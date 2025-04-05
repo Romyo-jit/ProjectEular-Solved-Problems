f4045323c42b970135920fae5a40fe04bc67fe9c
Romyojit Paul <rabinpaul110@gmail.com>
2025-04-05 23:38:01 +0530

    First Commit

---
+++ 3_Largest_Prime_Factor.py
@@ -0,0 +1,22 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143?
+
+num = 600851475143
+limit = int(num**(1/2))
+fctors = []
+
+for i in range(2, limit+1):
+    if num%i == 0:
+        fctors.append(i)
+
+pfac = []
+for i in fctors:
+    run = True
+    for j in range(2, int(i**(1/2)) + 1):
+        if i%j == 0:
+            run = False
+            break
+    if run:
+        pfac.append(i)
+
+print(max(pfac))
