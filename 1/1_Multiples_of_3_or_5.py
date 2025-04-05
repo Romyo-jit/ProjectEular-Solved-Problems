b45c07ed741d0da55d3125930494193a3772efdb
Romyojit Paul <rabinpaul110@gmail.com>
2025-04-06 00:18:18 +0530

    modified:   1/1_Multiples_of_3_or_5.py

--- 1/1_Multiples_of_3_or_5.py
+++ 1/1_Multiples_of_3_or_5.py
@@ -6,5 +6,4 @@ for i in range(1000):
     if (i%3 == 0) or (i%5 == 0):
         sum = sum + i
 
-print("The sum of all the multiples of 3 or 5 below 1000 is:")
-print(sum)
+print("The sum of all the multiples of 3 or 5 below 1000 is:", sum)
